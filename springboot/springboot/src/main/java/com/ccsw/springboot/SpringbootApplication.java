df13ae0ee33b67310cbe1cee698f30fba57e92c6
ivanmr14 <imitro@alumni.uv.es>
2023-12-13 10:51:37 +0100

    Implemento zona horaria
    Implemento zona horaria para que coincida con la zona horaria de angular

--- springboot/springboot/src/main/java/com/ccsw/springboot/SpringbootApplication.java
+++ springboot/springboot/src/main/java/com/ccsw/springboot/SpringbootApplication.java
@@ -1,13 +1,27 @@
 package com.ccsw.springboot;
 
+import java.util.TimeZone;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringbootApplication.class);
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+1"));
+        logger.info("Zona horaria configurada: {}", TimeZone.getDefault().getID());
+    }
 
 }
